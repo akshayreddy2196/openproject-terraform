13bd2262f7d7fd3966fb79a5a6213f5dfbec98dc
akshayreddy2196 <77155965+akshayreddy2196@users.noreply.github.com>
2025-09-09 12:36:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -137,13 +137,7 @@ resource "null_resource" "openproject_setup" {
       "sudo usermod -aG docker ec2-user",
       "sg docker -c",
       "sudo mkdir -p /var/lib/openproject/{pgdata,assets}",
-      "sudo docker run -d --name openproject -p 8080:80",
-      "-e OPENPROJECT_HOST_NAME=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)",
-      "-e OPENPROJECT_SECRET_KEY_BASE=$(openssl rand -hex 32)",
-      "-e OPENPROJECT_HTTPS=false ",
-      "-v /var/lib/openproject/pgdata:/var/openproject/pgdata",
-      "-v /var/lib/openproject/assets:/var/openproject/assets",
-      "openproject/openproject:16"
+      "sudo docker run -d --name openproject -p 8080:80 -e OPENPROJECT_HOST_NAME=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4) -e OPENPROJECT_SECRET_KEY_BASE=$(openssl rand -hex 32) -e OPENPROJECT_HTTPS=false -v /var/lib/openproject/pgdata:/var/openproject/pgdata -v /var/lib/openproject/assets:/var/openproject/assets openproject/openproject:16"
     ]
   }
 }
