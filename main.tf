6eaee75a14998b7dd6b512f8bebaf604eccb9ab4
akshayreddy2196 <77155965+akshayreddy2196@users.noreply.github.com>
2025-09-09 11:38:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -135,7 +135,8 @@ resource "null_resource" "openproject_setup" {
       "sudo systemctl enable docker",
       "sudo systemctl start docker",
       "sudo usermod -aG docker ec2-user",
-      "newgrp docker",
+      "sg docker -c",
+      "sudo docker run -d --name openproject -p 8080:80",
       "-e OPENPROJECT_HOST_NAME=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)",
       "-e OPENPROJECT_SECRET_KEY_BASE=$(openssl rand -hex 32)",
       "-e OPENPROJECT_HTTPS=false ",
@@ -162,7 +163,7 @@ resource "aws_lb" "alb" {
 
 resource "aws_lb_target_group" "tg" {
   name     = "openproject-tg"
-  port     = 80
+  port     = 8080
   protocol = "HTTP"
   vpc_id   = aws_vpc.main.id
 
@@ -190,5 +191,5 @@ resource "aws_lb_listener" "http" {
 resource "aws_lb_target_group_attachment" "ec2_attach" {
   target_group_arn = aws_lb_target_group.tg.arn
   target_id        = aws_instance.openproject.id
-  port             = 80
+  port             = 8080
 }
